cbfdf1793d46997f72321ba80249292e83f11b1d
anviaan <andre.carbajal.vargas@gmail.com>
2022-03-17 10:10:33 -0500

    1.17.1 port

--- src/main/java/net/anvian/bedrockplus/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/anvian/bedrockplus/world/feature/ModConfiguredFeatures.java
@@ -5,23 +5,28 @@ import net.anvian.bedrockplus.block.ModBlocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.*;
-
-import java.util.List;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 
 public class ModConfiguredFeatures {
-    public static final List<OreFeatureConfig.Target> OVERWORLD_IMPURE_BEDROCK_ORES = List.of(
-            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.IMPURE_BEDROCK.getDefaultState()));
+    public static  final RegistryKey<ConfiguredFeature<?,?>> IMPURE_BEDROCK_KEY =
+            registerKey("impure_bedrock_ore");
 
-    public static final ConfiguredFeature<?, ?> IMPURE_BEDROCK_ORE = register("impure_bedrock_ore",
-            //maximum amount shown together
-            Feature.ORE.configure(new OreFeatureConfig(OVERWORLD_IMPURE_BEDROCK_ORES, 5)));
+    public static final ConfiguredFeature<?, ?> IMPURE_BEDROCK_ORE = register(Feature.ORE.configure(
+                    new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, ModBlocks.IMPURE_BEDROCK.getDefaultState(), 5))//vein size
+            .range(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.getBottom(), YOffset.fixed(5))))
+            .spreadHorizontally().repeat(55), IMPURE_BEDROCK_KEY);// veins per chunk
 
+    private static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(BedrockPlusMod.MOD_ID, name));
+    }
 
-
-    public static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String name, ConfiguredFeature<FC, ?> configuredFeature) {
-        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(BedrockPlusMod.MOD_ID, name),
-                configuredFeature);
+    private static ConfiguredFeature<?, ?> register(ConfiguredFeature<?, ?> configuredFeature,
+                                                    RegistryKey<ConfiguredFeature<?, ?>> key) {
+        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, key.getValue(), configuredFeature);
     }
 
     public static void registerConfiguredFeatures() {
--- src/main/java/net/anvian/bedrockplus/world/gen/ModOreGeneration.java
+++ src/main/java/net/anvian/bedrockplus/world/gen/ModOreGeneration.java
@@ -1,6 +1,6 @@
 package net.anvian.bedrockplus.world.gen;
 
-import net.anvian.bedrockplus.world.feature.ModPlacedFeatures;
+import net.anvian.bedrockplus.world.feature.ModConfiguredFeatures;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.world.gen.GenerationStep;
@@ -8,6 +8,6 @@ import net.minecraft.world.gen.GenerationStep;
 public class ModOreGeneration {
     public static void generateOres() {
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
-                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.IMPURE_BEDROCK_ORE_PLACED_KEY);
+                GenerationStep.Feature.UNDERGROUND_ORES, ModConfiguredFeatures.IMPURE_BEDROCK_KEY);
     }
 }
